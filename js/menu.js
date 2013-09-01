f33598f04f588decb4ab561c9cc911b6a695d9a5
Arpan Chinta <arpancj@gmail.com>
2013-09-01 22:04:20 +0530

    Add responsive menu layout & js

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+jQuery(function(){
+  $('nav').click(function(){
+    if (!$(this).hasClass('active')) {
+      $(this).addClass('active');    
+    };
+  });
+
+  $('nav li span').click(function(event){
+    $(this).parent('li').siblings().removeClass('active');
+    $(this).parent('li').toggleClass('active');
+    event.stopPropagation();
+  })
+
+})
