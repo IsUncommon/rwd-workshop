a37a9f388da45f20aab12c4a99a7a5c6de5161b0
Arpan Chinta <arpancj@gmail.com>
2013-09-02 21:01:56 +0530

    Adding slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  setInterval(slideSwitch, 3000);
+});
+
+
+slideSwitch = function(){
+  $current = $('.slideshow li.current');
+
+  $next = $current.next();
+  if ($next.length == 0) {
+    $next = $('.slideshow li:first-child');
+  };
+  
+  $next.addClass('current');
+  $current.removeClass('current').addClass('previous');
+
+  window.setTimeout(function(){
+    $current.removeClass('previous');
+  }, 2000);
+}
